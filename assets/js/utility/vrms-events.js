2f7f6b7a0efd903d8e774779c894866a5bad3130
Gayle Martin <76500899+gaylem@users.noreply.github.com>
2024-05-14 07:34:47 -0500

    Update vrms-events.js to test codeql

--- assets/js/utility/vrms-events.js
+++ assets/js/utility/vrms-events.js
@@ -1,6 +1,8 @@
 ---
 ---
 
+const = "test"
+    
 {% assign vrmsData = site.data.external.vrms_data %}
 const vrmsData = JSON.parse(decodeURIComponent("{{ vrmsData | jsonify | uri_escape }}"));
 
@@ -14,7 +16,6 @@ export const vrmsDataFetch = (currentPage, appendMeetingTimes) => {
 
  // Helper function used for project.html and right-col-content.html to sort VRMS data by day of the week from "date" key and meeting time from "startTime" key
  function sortByDate(scheduleData, currentPage, appendMeetingTimes) {
-    {% assign vrmsData = site.data.external.vrms_data %}
     const map = {
         'Mon': 1,
         'Tue': 2,
